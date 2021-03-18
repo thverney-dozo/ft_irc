c03bab51381f09ea2be051034380b85f9f973e6f
Thverney-Dozo <theo7420@hotmail.fr>
2021-03-18 20:10:59 +0100

    fix channel class

--- Channel.hpp
+++ Channel.hpp
@@ -6,49 +6,58 @@
 #include <unistd.h>
 #include "Client.hpp"
 
-class Channel{
+class Channel
+{
 	private:
-	std::string _name;
-	std::list<Client> _clients;
-	Channel(const Channel &other);
-	Channel &operator=(const Channel &other);
+		std::string _name;
+		std::list<Client*> _clients;
+
+		Channel();
+		Channel(const Channel &other);
+		Channel &operator=(const Channel &other);
+
 	public:
-	Channel(std::string name, Client client);
-	std::string getChanName() const;
-	std::list<Client> getConnectedClients() const;
-	void addClient(Client client);
-	void clientWriteMsg(std::string msg, Client client);
-	virtual ~Channel();
+		Channel(std::string const &name, Client &client);
+		virtual ~Channel();
+
+		void addClient(Client *client);
+		void clientWriteMsg(std::string msg, Client *client);
+
+		std::string getChanName() const;
+		std::list<Client*> getConnectedClients() const;
 };
 
-Channel::Channel(std::string name, Client client)
+Channel::Channel(std::string const &name, Client &client)
 {
 	_name = name;
-	_clients.push_back(client);
+	_clients.push_back(&client);
 }
+
+Channel::~Channel()
+{}
 
 std::string Channel::getChanName() const
 {
 	return _name;
 }
 
-std::list<Client> Channel::getConnectedClients() const
+std::list<Client*> Channel::getConnectedClients() const
 {
 	return _clients;
 }
 
-void Channel::addClient(Client client)
+void Channel::addClient(Client *client)
 {
 	_clients.push_back(client);
 }
 
-void Channel::clientWriteMsg(std::string msg, Client client)
+void Channel::clientWriteMsg(std::string msg, Client *client)
 {
-	std::list<Client>::iterator end = _clients.end();
-	for (std::list<Client>::iterator begin = _clients.begin(); begin != end; begin++)
+	std::list<Client*>::iterator end = this->_clients.end();
+	for (std::list<Client*>::iterator begin = this->_clients.begin(); begin != end; begin++)
 	{
-		std::string str = _name + ": " + client.getName() + ": " + msg;
-		write((*begin).getFd(), str.c_str(), str.size()); 
+		std::string str = _name + ": " + client->getName() + ": " + msg;
+		write((*begin)->getFd(), str.c_str(), str.size()); 
 	}
 }
 
